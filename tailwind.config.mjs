ef55845f1903443db08cab2f5c54625e48bd3f88
Abraham Gutierrez <labraham1986@gmail.com>
2024-02-07 18:04:12 -0500

    update tailwind.config file

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -60,28 +60,28 @@ export default {
       },
       backgroundImage: {
         //Background
-        "home-mobile": "url('./assets/home/background-home-mobile.jpg')",
-        "home-tablet": "url('./assets/home/background-home-tablet.jpg')",
-        "home-desktop": "url('./assets/home/background-home-desktop.jpg')",
+        "home-mobile": "url('/assets/home/background-home-mobile.jpg')",
+        "home-tablet": "url('/assets/home/background-home-tablet.jpg')",
+        "home-desktop": "url('/assets/home/background-home-desktop.jpg')",
         //Destination
         // "url('./src/assets/destination/background-destination-mobile.jpg')",
         "destination-mobile":
-          "url('./assets/destination/background-destination-mobile.jpg')",
+          "url('/assets/destination/background-destination-mobile.jpg')",
         "destination-tablet":
-          "url('./assets/destination/background-destination-tablet.jpg')",
+          "url('/assets/destination/background-destination-tablet.jpg')",
         "destination-desktop":
-          "url('./assets/destination/background-destination-desktop.jpg')",
+          "url('/assets/destination/background-destination-desktop.jpg')",
         //Crew
-        "crew-mobile": "url('./assets/crew/background-crew-mobile.jpg')",
-        "crew-tablet": "url('./assets/crew/background-crew-tablet.jpg')",
-        "crew-desktop": "url('./assets/crew/background-crew-desktop.jpg')",
+        "crew-mobile": "url('/assets/crew/background-crew-mobile.jpg')",
+        "crew-tablet": "url('/assets/crew/background-crew-tablet.jpg')",
+        "crew-desktop": "url('/assets/crew/background-crew-desktop.jpg')",
         //Technology
-        "tech-mobile": "url('./assets/technology/background-technology-mobile.jpg')",
-        "tech-tablet": "url('./assets/technology/background-technology-tablet.jpg')",
-        "tech-desktop": "url('./assets/technology/background-technology-desktop.jpg')",
+        "tech-mobile": "url('/assets/technology/background-technology-mobile.jpg')",
+        "tech-tablet": "url('/assets/technology/background-technology-tablet.jpg')",
+        "tech-desktop": "url('/assets/technology/background-technology-desktop.jpg')",
         //Technology
-        "404-mobile": "url('./assets/404/mobile.webp')",
-        "404-tablet": "url('./assets/404/tablet.webp')",
+        "404-mobile": "url('/assets/404/mobile.webp')",
+        "404-tablet": "url('/assets/404/tablet.webp')",
       },
       padding: {
         "124px": "124px",
